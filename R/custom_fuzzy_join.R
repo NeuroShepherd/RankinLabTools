49dfeb94acb0829d424d77101ac93ca81c751948
Patrick Callahan <patricktcallahan18@gmail.com>
2021-10-05 20:02:26 -0700

    Update the fuzzy join function to use more stable rlang and dplyr syntax

--- R/custom_fuzzy_join.R
+++ R/custom_fuzzy_join.R
@@ -2,42 +2,47 @@
 #' Title
 #'
 #' @param df1 first dataframe argument
-#' @param DCDate1 first date argument
-#' @param PIDN1 first ID argument; defaults to PIDN
 #' @param df2 second dataframe argument
-#' @param DCDate2 second date argument
-#' @param PIDN2 second ID argument; defaults to PIDN
 #' @param mode used to specify if the join should be "left", "right", "full", "semi", or "anti"; defaults to "left"
 #' @param interval absolute value of days over which to match visits; default of +/-90 days
+#' @param key1
+#' @param key2
+#' @param date1
+#' @param date2
 #'
 #' @return
 #' @export
 #'
 #' @examples
-custom_fuzzy_join <- function(df1, DCDate1, PIDN1 = PIDN,
-                              df2, DCDate2, PIDN2 = PIDN,
-                              mode = "left", interval = 90) {
+custom_fuzzy_join <- function(df1, df2, key1, key2, date1, date2, mode = "left", interval = 90) {
 
-  PIDN1 <- enquo(PIDN1)
-  PIDN2 <- enquo(PIDN2)
-  DCDate1 <- enquo(DCDate1)
-  DCDate2 <- enquo(DCDate2)
 
-  joining_pidn <- set_names(quo_name(PIDN2),quo_name(PIDN1))
-  joining_date <- set_names(quo_name(DCDate2),quo_name(DCDate1))
+  joining_pidn <- magrittr::set_names(rlang::as_name(rlang::enquo(PIDN2)),
+                            rlang::as_name(rlang::enquo(PIDN1)) )
 
-  DCDate1 <- df1 %>%
-    select(!!DCDate1) %>%
-    mutate_at(vars(!!DCDate1), ~as_date(.))
-  DCDate2 <- df2 %>%
-    select(!!DCDate2) %>%
-    mutate_at(vars(!!DCDate2), ~as_date(.))
+  joining_date <- magrittr::set_names(rlang::as_name(rlang::enquo(DCDate2)),
+                            rlang::as_name(rlang::enquo(DCDate1)) )
 
-  df1 %>%
+
+  DCDate1 <- df1 %>% dplyr::select({{DCDate1}}) %>% dplyr::mutate(dplyr::across(~as.date(.)))
+
+  DCDate2 <- df2 %>% dplyr::select({{DCDate2}}) %>% dplyr::mutate(dplyr::across(~as.date(.)))
+
+
+  fuzzy_joined_data <- df1 %>%
     fuzzyjoin::fuzzy_join(df2,
                           by=c(joining_pidn,joining_date),
                           match_fun = list(`==`, function(x,y)abs(x-y)<interval),
                           mode=mode)
 
+  return(fuzzy_joined_data)
+
 }
 
+
+# Sub into the fuzzy_join by= call?
+rlang::as_name(rlang::enquo(PIDN1)) = rlang::as_name(rlang::enquo(PIDN2))
+rlang::as_name(rlang::enquo(DCDate1)) = rlang::as_name(rlang::enquo(DCDate2))
+
+# If the above doesn't work, then at least update the quo_name to as_name and
+# put the enquo calls into the set_names stuff
