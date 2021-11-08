1bc1c0928e83cfdf556058aaa2c724aa82c12898
patrick-callahan <patrick.callahan@ucsf.edu>
2021-11-08 11:18:18 -0800

    Remove old comments on filter_if_all_na.R function

--- R/filter_if_all_na.R
+++ R/filter_if_all_na.R
@@ -19,13 +19,7 @@ filter_if_all_na <- function(dataframe, ...){
     dplyr::filter( dplyr::if_any(c(...), ~!is.na(.)) )
 
   return(dataframe)
-  # This function allows you to remove rows from a dataframe given ALL variables specified in the *columns*
-  # argument are NA.
 
-  # Should rename this function to drop_if_all_NA()
-  # Should create another version of the function where a row is dropped if any variables in the list are
-  # missing, and this would be drop_if_any_NA(). It's counterintuitive, but I think I would just need to
-  # change the any_vars() call to all_vars() in the function to implement this change.
 }
 
 
