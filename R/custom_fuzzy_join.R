6bbe44d1c2a66c5c30d23f633b49fbbdb8d96cbd
patrick-callahan <patrick.callahan@ucsf.edu>
2021-11-08 10:59:50 -0800

    Adjust fuzzy join code

--- R/custom_fuzzy_join.R
+++ R/custom_fuzzy_join.R
@@ -16,27 +16,23 @@
 #' @export
 #'
 #' @examples
-custom_fuzzy_join <- function(df1, DCDate1, PIDN1 = PIDN,
-                              df2, DCDate2, PIDN2 = PIDN,
+join_key_date_difference <- function(data1, data2, id1, id2, date1, date2,
                               mode = "left", interval = 90) {
 
-  PIDN1 <- enquo(PIDN1)
-  PIDN2 <- enquo(PIDN2)
-  DCDate1 <- enquo(DCDate1)
-  DCDate2 <- enquo(DCDate2)
 
-  joining_pidn <- set_names(quo_name(PIDN2),quo_name(PIDN1))
-  joining_date <- set_names(quo_name(DCDate2),quo_name(DCDate1))
+  # Sub into the fuzzy_join by= call?
+  # rlang::as_name(rlang::enquo(PIDN1)) = rlang::as_name(rlang::enquo(PIDN2))
+  # rlang::as_name(rlang::enquo(DCDate1)) = rlang::as_name(rlang::enquo(DCDate2))
 
-  DCDate1 <- df1 %>%
-    select(!!DCDate1) %>%
-    mutate_at(vars(!!DCDate1), ~as_date(.))
-  DCDate2 <- df2 %>%
-    select(!!DCDate2) %>%
-    mutate_at(vars(!!DCDate2), ~as_date(.))
+  joining_pidn <- magrittr::set_names(rlang::as_name(rlang::enquo(id2)),
+                                      rlang::as_name(rlang::enquo(id1)) )
 
-  df1 %>%
-    fuzzyjoin::fuzzy_join(df2,
+  joining_date <- magrittr::set_names(rlang::as_name(rlang::enquo(date2)),
+                                      rlang::as_name(rlang::enquo(date1)) )
+
+
+  data1 %>%
+    fuzzyjoin::fuzzy_join(data2,
                           by=c(joining_pidn,joining_date),
                           match_fun = list(`==`, function(x,y)abs(x-y)<interval),
                           mode=mode)
