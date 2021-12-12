ae7116aaa96ca010699b3495cee45bf4c7df6b19
Patrick Callahan <patricktcallahan18@gmail.com>
2021-12-11 17:37:27 -0800

    Add numeric check and warning message to negative values to NA function

--- R/set_negative_values_to_na.R
+++ R/set_negative_values_to_na.R
@@ -14,9 +14,33 @@
 #' @examples
 negative_values_to_na <- function(.data, ...){
 
+  non_numeric_vars <- select(.data, ...) %>%
+    map(~is.numeric(.x)) %>%
+    keep(. == FALSE) %>% names %>%
+    toString()
+
+  if (length(non_numeric_vars) > 0) {
+    stop(glue::glue("Cannot operate on non-numeric variables: {non_numeric_vars}"))
+  }
+
   .data %<>%
-    dplyr::mutate(dplyr::across(c(...), ~dplyr::case_when(. >= 0 ~ .)) )
+    dplyr::mutate(dplyr::across(c(...), ~dplyr::case_when(. >= 0 ~ .,
+                                                          TRUE ~ NA_real_)
+                                )
+                  )
 
   return(.data)
 
 }
+
+
+
+
+mtcars %>%
+  mutate(cyl = as.character((cyl)),
+         mpg = as.character(mpg)) %>% tibble %>%
+  negative_values_to_na(mpg:hp)
+
+
+
+
