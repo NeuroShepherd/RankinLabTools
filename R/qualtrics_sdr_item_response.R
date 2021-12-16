af9c6b6395fbd766b425edd6dcba331e1cd415a6
patrick-callahan <patrick.callahan@ucsf.edu>
2021-12-15 16:49:18 -0800

    Update arguments and documentation for the sdr item response variable

--- R/qualtrics_sdr_item_response.R
+++ R/qualtrics_sdr_item_response.R
@@ -15,7 +15,7 @@
 #'
 #' ```
 #'
-qualtrics_sdr_item_response <- function(.data, variable) {
+qualtrics_sdr_item_response <- function(.data, root_variable) {
 
   var_names <- .data %>%
     select(matches({{root_variable}})) %>%
@@ -23,7 +23,7 @@ qualtrics_sdr_item_response <- function(.data, variable) {
     sort()
 
   result <- .data %>%
-    mutate("response_{variable}" := case_when(.data[[ var_names[[1]] ]] %in% c("On","Like") ~ 1,
+    mutate("response_{root_variable}" := case_when(.data[[ var_names[[1]] ]] %in% c("On","Like") ~ 1,
                                               .data[[ var_names[[2]] ]] %in% c("On","Like") ~ 2,
                                               .data[[ var_names[[3]] ]] %in% c("On","Like") ~ 3,
                                               .data[[ var_names[[4]] ]] %in% c("On","Like") ~ 4,
