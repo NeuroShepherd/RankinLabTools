d0aa724e3d5c9a2b1be2b6afea16548e80e9fa6c
patrick-callahan <patrick.callahan@ucsf.edu>
2021-12-16 11:04:45 -0800

    Overhaul qualtrics sdr item score function to accept a metadata file, and to use 3 columns from the metadata file for the 2x2point and 1x1point vectors

--- R/qualtrics_sdr_item_score.R
+++ R/qualtrics_sdr_item_score.R
@@ -17,20 +17,32 @@
 #'
 #' ```
 #'
-qualtrics_sdr_item_score <- function(.data, two_point1, two_point2 = NA, one_point) {
+qualtrics_sdr_item_score <- function(.data, .metadata, two_point1, two_point2 = NA, one_point) {
 
-  var <- .data %>%
-    select({{two_point1}}) %>%
-    names() %>%
-    str_remove_all(., "_[:digit:].*") %>%
-    paste0("sdr_score_",.)
+  # https://stackoverflow.com/questions/57136322/what-does-the-operator-mean-in-r-particularly-in-the-context-symx
 
+  two_point1_sym <- pull(.metadata, {{two_point1}}) %>%
+    map( ~{ if (is.na(.x)) { NA } else { sym(.x) } } )
+  two_point2_sym <- pull(.metadata, {{two_point2}}) %>%
+    map( ~{ if (is.na(.x)) { NA } else { sym(.x) } } )
+  one_point_sym <- pull(.metadata, {{one_point}}) %>%
+    map( ~{ if (is.na(.x)) { NA } else { sym(.x) } } )
 
-  .data %>%
-    mutate(
-      {{var}} := case_when( {{two_point1}} == "On" | {{two_point2}} == "On" ~ 2,
-                            {{one_point}} == "On" ~ 1,
-                            TRUE ~ 0)
-    )
+  names <- pull( .metadata, {{two_point1}} ) %>%
+    map( ~str_remove_all(.x, "_[:digit:].*") %>% paste0("sdr_score_",.) )
+
+  meta_vars <- list(two_point1_sym, two_point2_sym, one_point_sym, names)
+
+  pmap(meta_vars,
+       function(two_point1_sym, two_point2_sym, one_point_sym, names) {
+         .data %>%
+           mutate({{names}} := case_when( !!two_point1_sym == "On" | !!two_point2_sym == "On" ~ 2,
+                                          !!one_point_sym == "On" ~ 1,
+                                          TRUE ~ 0) ) %>%
+           select(last_col())
+       }
+  ) %>%
+    bind_cols()
+
 
 }
