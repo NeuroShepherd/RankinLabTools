34ae61473f9ae729136a29a5a86f3e0dedc8b31a
Patrick Callahan <patricktcallahan18@gmail.com>
2022-09-13 12:33:30 -0700

    Import the magrittr assignment pipe

---
+++ R/utils-assignment-pipe.R
@@ -0,0 +1,14 @@
+#' Assignment pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:assignment_pipe]{\%<>\%}} for details.
+#'
+#' @name %<>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %<>%
+#' @usage lhs \%<>\% rhs
+#' @param lhs An object which serves both as the initial value and as target.
+#' @param rhs a function call using the magrittr semantics.
+#' @return The result of calling `rhs(lhs)`
+NULL
